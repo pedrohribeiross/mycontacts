3ec06e0569f5e94893430d3e3736805c60bdedff
Pedro <pedro_henrique.rss@hotmail.com>
2023-10-10 06:43:57 -0300

    feat: create Button component

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -13,6 +14,14 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+
+      <Button type="button">
+        Salvar alteração
+      </Button>
+
+      <Button type="button" disabled>
+        Salvar alteração
+      </Button>
     </>
   );
 }

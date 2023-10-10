7f48d8ce655722408ee3a2ddfd822f5d58773639
Pedro <pedro_henrique.rss@hotmail.com>
2023-10-10 06:35:26 -0300

    feat: create Input and Select components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,18 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
-  return <PageHeader title="Novo contato" />;
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }

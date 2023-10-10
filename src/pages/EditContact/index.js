49dc429608f496d1f78651c2d4aefbe22297cdd0
Pedro <pedro_henrique.rss@hotmail.com>
2023-10-10 06:22:33 -0300

    feat: create pageHeader component

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,3 +1,5 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
-  return <h1>EditContact</h1>;
+  return <PageHeader title="Editar contato" />;
 }
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,5 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
-  return <h1>NewContact</h1>;
+  return <PageHeader title="Novo contato" />;
 }
